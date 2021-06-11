86939691ad1678a418486d0441a2c356bf1ab522
Rodolfo <73254069+RodolfoParada@users.noreply.github.com>
2021-06-10 21:00:48 -0400

    Add files via upload

---
+++ EjercicioJava1/src/ejerciciojava1/EjercicioJava1.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciojava1;
+
+/**
+ *
+ * @author Rodolfo
+ */
+public class EjercicioJava1 {
+
+    /**
+     - byte
+     - short
+     - int
+     - long
+     - char
+     - double
+     - float
+     - String
+     - boolean
+     */
+    
+    
+    public static void main(String[] args) {
+       byte a;
+       short b = 200;
+       int c = 1000000;
+       long d = 100000000000000L;
+       char e = 'F';
+       double f= 4.5;
+       float g = 4.5F;
+       String h = "Cadena de Caracteres"; 
+       boolean i = false ;//solo se usa false or true      
+               
+    }
+    
+}
