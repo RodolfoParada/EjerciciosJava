3a54605b817e95e705169fd85c8f263c7926a687
Rodolfo <73254069+RodolfoParada@users.noreply.github.com>
2021-11-03 19:28:36 -0300

    Add files via upload

---
+++ EjercicioJava44/src/ejerciciojava44/EjercicioJava44.java
@@ -0,0 +1,30 @@
+package ejerciciojava44;
+
+public class EjercicioJava44 {
+    /**
+    Crea una matriz de números de 3x3 tu mismo.
+    Crea una función que pasandole una matriz y una fila(número)
+    muestre solo esa fila.
+     */
+    public static void main(String[] args) {
+        
+        int [][] matriz =
+        {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9} 
+        };
+        muestraFilaMatriz(matriz,2);
+}
+    public static void muestraFilaMatriz (int[][] matriz, int fila){
+        if (fila>=0 && fila<matriz.length){    
+              for (int j = 0; j < matriz[0].length; j++){
+         
+                  System.out.println(matriz[fila][j]);
+              }
+        }else{
+            System.out.println("La fila no es correcta");
+            
+        }
+    }
+}
