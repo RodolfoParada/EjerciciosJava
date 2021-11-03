70143588c0eb83aec699ee4972921c7f52803b1d
Rodolfo <73254069+RodolfoParada@users.noreply.github.com>
2021-11-03 19:33:20 -0300

    Add files via upload

---
+++ EjercicioJava46/src/ejerciciojava46/EjercicioJava46.java
@@ -0,0 +1,28 @@
+
+package ejerciciojava46;
+
+public class EjercicioJava46 {
+
+    /**
+    Crea una matriz de números de 3x3 y recorre su diagonal principal 
+    y su diagonal inversa
+     */
+    public static void main(String[] args) {
+       int [][]matriz =
+       {
+           {1,2,3},
+           {4,5,6},
+           {7,8,9}
+       };
+        System.out.println("Diagonal principal");
+       for (int i = 0; i < matriz.length; i++){
+          System.out.println(matriz[i][i]);    
+       
+    }
+      System.out.println("Diagonal inversa");
+       for (int i = 0, j=matriz[0].length-1; i < matriz.length; i++, j--) {
+          System.out.println(matriz[i][j]);    
+    
+}
+}
+}
